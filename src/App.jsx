a977e012487601402102e7901b112b6292ca3a98
Ramom Gomes <ramomusica09@hotmail.com>
2023-08-08 20:19:58 -0300

    laola

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,6 @@ const ProjetoAcademia = () => {
   return (
     <div>
       <h1>APENAS COMEÇE.</h1>
-      <h1>APENAS COMEÇE.</h1>
-      <h1>APENAS COMEÇE.</h1>
-      <h1>APENAS COMEÇE.</h1>
     </div>
   );
 }
