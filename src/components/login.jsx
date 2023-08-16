e843f1a497032c89de45104359128a7f185f80de
Ramom Gomes <ramomusica09@hotmail.com>
2023-08-15 21:15:09 -0300

    encaminhar login para registro

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom"; // Importe o componente Link
+
 
 function LoginPage({ registerUser }) {
   const [email, setEmail] = useState("");
@@ -14,6 +16,9 @@ function LoginPage({ registerUser }) {
       <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
       <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
       <button onClick={handleLogin}>Login</button>
+      <p>
+        Ainda não tem um login? <Link to="/registro">Criar meu login</Link>
+      </p>
     </div>
   );
 }
