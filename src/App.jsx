31b4bf5bc7b9d8d62c8e835c550e9ef8053ecbfc
Ramom Gomes <rgs.ramomgomes@hotmail.com>
2023-09-04 21:44:23 -0300

    menu-header-inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import Rotas from './routes/rotas';
-
+import Header from './components/Header';
+import Menu from './components/Menu';
 
 function App() {
   const [users, setUsers] = useState([]);
+  const location = useLocation();
 
   useEffect(() => {
     const storedUsers = JSON.parse(localStorage.getItem('users')) || [];
@@ -18,11 +21,21 @@ function App() {
     setUsers((prevUsers) => [...prevUsers, newUser]);
   };
 
+  const hideHeaderAndMenu = location.pathname === '/' || 
+  location.pathname === '/registro' || 
+  location.pathname === '/trocar-senha';
+
   return (
     <div>
+      {!hideHeaderAndMenu && (
+        <div>
+          <Menu />
+        </div>
+      )}
+      {!hideHeaderAndMenu && <Header />}
       <Rotas users={users} registerUser={registerUser} />
     </div>
   );
 }
 
-export default App; 
+export default App;
