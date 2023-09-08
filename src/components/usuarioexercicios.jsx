ac594052b1384088403de6413ca1d948394e1836
Ramom Gomes <rgs.ramomgomes@hotmail.com>
2023-09-08 11:10:06 -0300

    remover-exercicio-usuario

--- src/components/usuarioexercicios.jsx
+++ src/components/usuarioexercicios.jsx
@@ -4,6 +4,21 @@ function UserExerciseList() {
   // Obtém o usuário logado do localStorage
   const currentUser = JSON.parse(localStorage.getItem('currentUser'));
 
+  // Função para remover um exercício da lista do usuário
+  const removeExercise = (index) => {
+    // Certifique-se de que o usuário e a lista de exercícios existem
+    if (currentUser && currentUser.exercises && currentUser.exercises.length > 0) {
+      // Remova o exercício pelo índice
+      currentUser.exercises.splice(index, 1);
+
+      // Atualize o usuário no localStorage
+      localStorage.setItem('currentUser', JSON.stringify(currentUser));
+
+      // Força a atualização do componente para refletir as mudanças
+      window.location.reload();
+    }
+  };
+
   return (
     <div>
       <h1>Exercícios Adicionados</h1>
@@ -21,6 +36,7 @@ function UserExerciseList() {
                 style={{ maxWidth: '100%', height: 'auto' }}
               />
               <p>Alvo: {exercise.target}</p>
+              <button onClick={() => removeExercise(index)}>Remover</button>
             </li>
           ))
         ) : (
