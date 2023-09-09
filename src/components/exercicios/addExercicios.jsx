b6e24b88a3ce9b757aff3a759a80f67c8330e104
Ramom Gomes <rgs.ramomgomes@hotmail.com>
2023-09-09 19:10:37 -0300

    criando-planos-inicio

--- src/components/exercicios/addExercicios.jsx
+++ src/components/exercicios/addExercicios.jsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css'; // Importe o arquivo de estilo
 
 function AddExerciseScreen({ exercise, onClose }) {
+  const [creatingPlan, setCreatingPlan] = useState(false);
+  const [newPlanName, setNewPlanName] = useState('');
+
+  const handleCreatePlan = () => {
+    setCreatingPlan(true);
+  };
+
+  const handleSavePlan = () => {
+    // Lógica para salvar o novo plano
+    const newPlan = { name: newPlanName, exercises: [exercise] }; // Inclui o exercício atual no plano
+
+    // Obtenha o usuário logado do localStorage
+    const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+
+    // Verifique se o usuário tem a propriedade "planos"
+    if (!currentUser.planos) {
+      currentUser.planos = [];
+    }
+
+    // Adicione o novo plano aos planos do usuário
+    currentUser.planos.push(newPlan);
+
+    // Atualize o usuário no localStorage
+    localStorage.setItem('currentUser', JSON.stringify(currentUser));
+
+    // Após a conclusão, redefina o estado
+    setCreatingPlan(false);
+    setNewPlanName('');
+  };
+
   const handleAddExercise = () => {
     // Lógica para adicionar o exercício ao usuário
     // ...
@@ -19,6 +49,20 @@ function AddExerciseScreen({ exercise, onClose }) {
         <p>Nome do Exercício: {exercise.name}</p>
         {/* Outros campos para adicionar informações do exercício */}
         <button onClick={handleAddExercise}>Adicionar</button>
+
+        {!creatingPlan ? (
+          <button onClick={handleCreatePlan}>Criar meu Plano</button>
+        ) : (
+          <div>
+            <input
+              type="text"
+              placeholder="Nome do Plano"
+              value={newPlanName}
+              onChange={(e) => setNewPlanName(e.target.value)}
+            />
+            <button onClick={handleSavePlan}>Salvar Plano</button>
+          </div>
+        )}
       </div>
     </div>
   );
