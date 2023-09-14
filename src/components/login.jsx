0aa7e3f08f071eae6f61d6973dc773068c060888
Ramom Gomes <rgs.ramomgomes@hotmail.com>
2023-09-13 21:30:36 -0300

    começo-estilização-login

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { FaSun, FaMoon } from 'react-icons/fa';
+import '../estilizações/login.css';
 
 function LoginPage({ users }) {
   const [email, setEmail] = useState("");
@@ -31,17 +33,20 @@ function LoginPage({ users }) {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      <button onClick={handleLogin}>Login</button>
-      {error && <p>{error}</p>}
-      <p>
-        Ainda não tem um login? <Link to="/registro">Criar meu login</Link>
-      </p>
-      <p>Esqueci minha senha! <Link to="/trocar-senha">Trocar Senha</Link></p>
-    </div>
+    <>
+      <main className={`main`}>
+        <h2>Login</h2>
+        <input className={`campo-email`} type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        {error && <p>{error}</p>}
+        <input className={`campo-senha`} type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        {error && <p>{error}</p>}
+        <div className={`links`}>
+          <Link className={`link-criarLogin`} to="/registro">Criar meu login</Link>
+          <Link className={`link-redefinirSenha`} to="/trocar-senha">Esqueci minha senha</Link>
+        </div>
+        <button className={`botao-login`} onClick={handleLogin}>Login</button>
+      </main>
+    </>
   );
 }
 
