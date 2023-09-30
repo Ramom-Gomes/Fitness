4034b5571f538efeab0e83b2f676d5bebdc9c653
Ramom Gomes <rgs.ramomgomes@hotmail.com>
2023-09-30 17:28:53 -0300

    menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { toggleMenu } from '../Redux/themeActions';
 import '../estilizações/menu.css';
 import { Link } from 'react-router-dom';
+import { BiUserCircle } from 'react-icons/bi';
 
 function Menu() {
 
   return (
     <div className="container-menu">
       <h1>Fitnes</h1>
-      <nav>
-        <Link to="/Home">Home</Link>
-        <Link to="/informacoes">Minhas informações</Link>
-        <Link to="/exercise-list">Exercícios</Link>
-        <Link to="meus-planos">Meus planos</Link>
+      <nav className='nav-list'>
+        <Link className='links' to="/Home">Home</Link>
+        <Link className='links' to="/exercise-list">Exercícios</Link>
+        <Link className='links' to="meus-planos">Meus planos</Link>
+        <Link className='link-user' to="/informacoes"><BiUserCircle size={30} color='white'/></Link>
       </nav>
     </div>
   );
