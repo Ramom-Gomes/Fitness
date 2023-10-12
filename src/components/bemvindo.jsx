7f76529449145e24d82b34284ba225d0ffbdf123
Ramom Gomes <rgs.ramomgomes@hotmail.com>
2023-10-12 17:17:55 -0300

    cssHome

--- src/components/bemvindo.jsx
+++ src/components/bemvindo.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { useSelector } from 'react-redux';
+import '../estilizações/bemvindo.css';
 
 function BemVindo() {
   const navigate = useNavigate();
@@ -27,16 +28,14 @@ function BemVindo() {
 
   return (
     <div className={`content ${menuOpen ? 'content-shifted' : ''}`}>
-      <div>
-        <h2>Olá, {userData.nome}!</h2>
-        <span>
-          <p>
+      <div className="containerHome">
+        <h2 className="tituloHome">Olá, {userData.nome}!</h2>
+        <span className="spanHome">
+          <p className="textoHome">
             Seja bem-vindo(a) ao nosso espaço dedicado ao seu bem-estar
             físico e mental. Aqui, você encontrará tudo o que precisa para
             alcançar seus objetivos de fitness de forma personalizada
             e eficaz.
-          </p>
-          <p>
             Crie planos de treino personalizados que se adaptem às suas
             necessidades específicas. Nossas ferramentas intuitivas 
             permitem que você monte seu próprio plano de treino ou escolha 
@@ -44,29 +43,29 @@ function BemVindo() {
             perfeito para alcançar seus objetivos.
           </p>
         </span>
-        <span>
-          <h3>Explore Nossos Recursos:</h3>
-          <p>
-            Exercícios Personalizados: Escolha os exercícios adequados para
+        <span className="spanHome">
+          <h3 className="subtituloHome">Explore Nossos Recursos:</h3>
+          <p className="textoHome">
+            Exercícios Personalizados: Escolha os <Link className="linksHome" to="/exercise-list">exercícios</Link> adequados para
             a sua meta e nível de condicionamento físico. Personalizamos
             cada plano para você. Planos de Treino: Crie e gerencie seus
-            próprios planos de treino ou escolha entre os que
+            próprios <Link className="linksHome" to="/meus-planos">planos de treino</Link> ou escolha entre os que
             oferecemos. Seu caminho para um corpo mais saudável começa aqui.
           </p>
         </span>
-        <span>
-          <h3>Pronto para Começar?</h3>
-          <p>
+        <span className="spanHome">
+          <h3 className="subtituloHome">Pronto para Começar?</h3>
+          <p className="textoHome">
             Se você está pronto para dar o próximo passo em direção a uma
-            vida mais saudável e ativa, clique abaixo para começar seu plano
-            de treino personalizado agora mesmo!Comece Seu Plano de
-            TreinoNão importa qual seja a sua jornada fitness, estamos
+            vida mais saudável e ativa, clique <Link className="linksHome" to="/exercise-list">aqui</Link> para visualizar nossos
+            exercícios e começar seu plano
+            de treino personalizado agora mesmo! Comece Seu Plano de
+            Treino Não importa qual seja a sua jornada fitness, estamos
             aqui para apoiar você em cada passo do caminho. Junte-se a
             nós e descubra o poder da transformação pessoal.
           </p>
         </span>
-        <Link to="/atualizar-usuario">Atualizar Informações</Link>
-        <button onClick={handleLogout}>Sair</button>
+        <button className="botaoHome" onClick={handleLogout}>Sair</button>
       </div>
     </div>
   );
