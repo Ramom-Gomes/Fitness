0569e83fa739d319095e4ccea1fb367eed142dd0
Ramom Gomes <rgs.ramomgomes@hotmail.com>
2023-10-30 20:55:53 -0300

    comeco-menu-responsivo

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import '../estilizações/menu.css';
 import { Link } from 'react-router-dom';
 import { BiUserCircle } from 'react-icons/bi';
+import { TfiMenu } from 'react-icons/tfi';
 
 function Menu() {
 
   return (
     <div className="container-menu">
       <h1 className='menuTitulo'>Fitnes</h1>
+      <nav className='menu-responsivo'>
+        <TfiMenu className='menu-logo'/>
+      </nav>
       <nav className='nav-list'>
         <Link className='links' to="/Home">Home</Link>
         <Link className='links' to="/exercise-list">Exercícios</Link>
