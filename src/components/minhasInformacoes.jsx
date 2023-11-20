5ef3f8d1dbdb704bfbf31fa5b9da507e365792d3
Ramom Gomes <rgs.ramomgomes@hotmail.com>
2023-11-20 15:15:13 -0300

    rotaUsuario

--- src/components/minhasInformacoes.jsx
+++ src/components/minhasInformacoes.jsx
@@ -1,101 +1,69 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import '../estilizações/informacoes.css';
+import { Link } from "react-router-dom";
 
 function AtualizarUsuario() {
   const navigate = useNavigate();
+  const [currentUser, setCurrentUser] = useState({});
 
-  const [currentUser, setCurrentUser] = useState({
-    email: "",
-    password: "",
-    nome: "",
-    idade: "",
-    peso: "",
-    palavraChave: "",
-    altura: "",
-    sexo: "",
-    objetivo: "",
-    nivelCondicionamento: "",
-    frequenciaTreino: ""
+  const [userStats, setUserStats] = useState({
+    planCount: 0,
+    exerciseCount: 0,
+    updateCount: 0,
   });
 
-  const [editMode, setEditMode] = useState(false);
-
-  // Função para obter o usuário atualizado do localStorage
-  const getCurrentUser = () => {
-    const storedUser = JSON.parse(localStorage.getItem('currentUser'));
-    if (storedUser) {
-      setCurrentUser(storedUser);
-    }
-  };
-
   useEffect(() => {
-    getCurrentUser();
-  }, []); // Chama a função uma vez quando o componente é montado
+    // Obtenha as informações do usuário do localStorage ou da API, se aplicável
+    const currentUser = JSON.parse(localStorage.getItem("currentUser")) || {};
+    setCurrentUser(currentUser);
+    
+    // Obtenha a contagem de planos do usuário
+    const userPlans = JSON.parse(localStorage.getItem("planos")) || {};
+    const planCount = Object.keys(userPlans[currentUser.email] || {}).length;
 
-  const handleUpdate = () => {
-    if (currentUser.email === "" || currentUser.password === "" || currentUser.nome === "") {
-      alert("Por favor, preencha todos os campos obrigatórios (email, senha, nome).");
-      return;
-    }
-
-    if (
-      isNaN(currentUser.idade) || 
-      isNaN(currentUser.peso) || 
-      isNaN(currentUser.altura) || 
-      currentUser.idade === "" || 
-      currentUser.peso === "" || 
-      currentUser.altura === ""
-    ) {
-      alert("Por favor, insira valores numéricos válidos para idade, peso e altura.");
-      return;
-    }
-
-    if (currentUser.palavraChave === "") {
-      alert("A palavra-chave não pode estar em branco.");
-      return;
-    }
-
-    const users = JSON.parse(localStorage.getItem("users")) || [];
-    const updatedUsers = users.map((u) =>
-      u.email === currentUser.email ? currentUser : u
+    // Obtenha a contagem total de exercícios nos planos do usuário
+    const exerciseCount = Object.values(userPlans[currentUser.email] || {}).reduce(
+      (total, plan) => total + (plan.exercises ? plan.exercises.length : 0),
+      0
     );
-    localStorage.setItem("users", JSON.stringify(updatedUsers));
-    localStorage.setItem("currentUser", JSON.stringify(currentUser));
-  
-    // Chama getCurrentUser para garantir que currentUser seja o mais atualizado
-    getCurrentUser();
-  
-    setEditMode(false);
-  };
+
+    // Obtenha a contagem de atualizações do usuário
+    const userEmail = currentUser.email;
+    const updatesObject = JSON.parse(localStorage.getItem("atualizacoes")) || {};
+    const updateCount = (updatesObject[userEmail] || []).length;
+
+    // Atualize o estado com as estatísticas do usuário
+    setUserStats({
+      planCount,
+      exerciseCount,
+      updateCount,
+    });
+  }, []); // Execute apenas uma vez no montagem do componente
 
   return (
-    <div>
-      <div className="containerInfo">
-        <h2 className="tituloInfo">Suas informações:</h2>
-        <label>Email: {editMode ? <input className="inputInfo" type="text" value={currentUser.email} onChange={e => setCurrentUser({...currentUser, email: e.target.value})} /> : currentUser.email}</label><br />
-        <label>Senha: {editMode ? <input className="inputInfo" type="password" value={currentUser.password} onChange={e => setCurrentUser({...currentUser, password: e.target.value})} /> : "******"}</label><br />
-        <label>Nome: {editMode ? <input className="inputInfo" type="text" value={currentUser.nome} onChange={e => setCurrentUser({...currentUser, nome: e.target.value})} /> : currentUser.nome}</label><br />
-        <label>Idade: {editMode ? <input className="inputInfo" type="text" value={currentUser.idade} onChange={e => setCurrentUser({...currentUser, idade: e.target.value})} /> : currentUser.idade}</label><br />
-        <label>Peso: {editMode ? <input className="inputInfo" type="text" value={currentUser.peso} onChange={e => setCurrentUser({...currentUser, peso: e.target.value})} /> : currentUser.peso}</label><br />
-        <label>Palavra-Chave: {editMode ? <input className="inputInfo" type="text" value={currentUser.palavraChave} onChange={e => setCurrentUser({...currentUser, palavraChave: e.target.value})} /> : currentUser.palavraChave}</label><br />
-        <label>Altura: {editMode ? <input className="inputInfo" type="text" value={currentUser.altura} onChange={e => setCurrentUser({...currentUser, altura: e.target.value})} /> : currentUser.altura}</label><br />
-        <label>Sexo: {editMode ? <input className="inputInfo" type="text" value={currentUser.sexo} onChange={e => setCurrentUser({...currentUser, sexo: e.target.value})} /> : currentUser.sexo}</label><br />
-        <label>Objetivo: {editMode ? <input className="inputInfo" type="text" value={currentUser.objetivo} onChange={e => setCurrentUser({...currentUser, objetivo: e.target.value})} /> : currentUser.objetivo}</label><br />
-        <label>Nível de Condicionamento: {editMode ? <input className="inputInfo" type="text" value={currentUser.nivelCondicionamento} onChange={e => setCurrentUser({...currentUser, nivelCondicionamento: e.target.value})} /> : currentUser.nivelCondicionamento}</label><br />
-        <label>Frequência de Treino: {editMode ? <input className="inputInfo" type="text" value={currentUser.frequenciaTreino} onChange={e => setCurrentUser({...currentUser, frequenciaTreino: e.target.value})} /> : currentUser.frequenciaTreino}</label><br />
-
-        <div className="botaoPosicaoInfo">
-          {editMode ? (
-            <div>
-              <button className="botaoInfo Atualizar " onClick={handleUpdate}>Atualizar</button>
-              <button className="botaoInfo" onClick={() => setEditMode(false)}>Cancelar</button>
-            </div>
-          ) : (
-            <button className="botaoInfo" onClick={() => setEditMode(true)}>Editar</button>
-          )}
-        </div>
-          <button className="botaoInfoVoltar" onClick={() => navigate(-1)}>Voltar</button>
+    <div className="containerInfo">
+      <div className="positionInputs">
+        <h2 className="tituloInfo">Seu Perfil:</h2>
+        <p className="Qt">Quantidade de Planos: {userStats.planCount}</p>
+        <p className="Qt">Quantidade de Exercícios nos Planos: {userStats.exerciseCount}</p>
+        <p className="Qt">Quantidade de Atualizações: {userStats.updateCount}</p>
+          <label className="Input">Nome: {currentUser.nome}</label><br />
+          <label className="Input">Idade: {currentUser.idade}</label><br />
+          <label className="Input">Peso: {currentUser.peso}</label><br />
+          <label className="Input">Palavra-Chave: {currentUser.palavraChave}</label><br />
+          <label className="Input">Altura: {currentUser.altura}</label><br />
+          <label className="Input">Sexo: {currentUser.sexo}</label><br />
+          <label className="Input">Objetivo: {currentUser.objetivo}</label><br />
+          <label className="Inputt">Nível de Condicionamento: {currentUser.nivelCondicionamento}</label><br />
+          <label className="Input">Frequência de Treino: {currentUser.frequenciaTreino}</label><br />
+        <button className="botaoInfoOfici" onClick={() => navigate(-1)}>
+          Voltar
+        </button>
+        <button className="botaoInfoOfici" onClick={() => navigate("/")}>
+          Sair
+        </button>
+        <p className="infoSemAtualizacoes">Para atualizar suas informações clique <Link className="linksHome" to="/atualizarInformacoes">aqui</Link></p>
       </div>
     </div>
   );
